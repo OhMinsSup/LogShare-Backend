123dbb1e5099b5e5484737d45627272cbca3ac6d
Your Name <you@example.com>
2019-04-24 16:11:15 +0900

    cookie bug fix (10)

--- src/lib/middleware/corsMiddleware.ts
+++ src/lib/middleware/corsMiddleware.ts
@@ -11,7 +11,12 @@ export default (ctx: Context, next: () => Promise<any>) => {
     ctx.set('Access-Control-Allow-Origin', 'http://localhost:4001');
   }
 
-  ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  ctx.set('Access-Control-Allow-Credentials', 'true');
+  ctx.set('Access-Control-Allow-Credentials', true as any);
+  ctx.set(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, x-timebase, Link'
+  );
+  ctx.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, OPTIONS');
+  ctx.set('Access-Control-Expose-Headers', 'Link');
   return next();
 };
